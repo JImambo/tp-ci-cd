2a162fea6e4cb1d893137c344950850ecee49308
JImambo <imambojonathan@gmail.com>
2026-04-20 15:24:47 +0200

    Ajout du script Python

---
+++ hello.py
@@ -0,0 +1,8 @@
+import datetime
+
+print("=" * 40)
+print("🔄 TP CI/CD - Premier pipeline")
+print("=" * 40)
+print(f"⏰ Heure d'exécution : {datetime.datetime.now()}")
+print("✅ Script exécuté avec succès !")
+print("=" * 40)
